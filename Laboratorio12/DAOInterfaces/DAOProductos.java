f86c1b4bb815152ec62017567544cd03169b591f
Odenfis <105258251+Odenfis@users.noreply.github.com>
2023-06-15 14:36:51 -0500

    Version2

--- Laboratorio12/DAOInterfaces/DAOProductos.java
+++ Laboratorio12/DAOInterfaces/DAOProductos.java
@@ -6,14 +6,9 @@ import javax.swing.JTable;
 public interface DaoProductos {
     
     public void registrar(Productos prod) throws Exception;
-    public String numeroCod() throws Exception;
-    
-    
-    public void llamarDatos(JTable visor) throws Exception;
-    
-    
-    public void eliminar(String id) throws Exception;
-    public void actualizar(Productos prod) throws Exception;
-    public void llamardatos2(JTable visor, String buscar) throws Exception;
+    public String mostrar() throws Exception;   
+    public void eliminar() throws Exception;  
+    public void buscar() throws Exception;
+    public void actualizar() throws Exception;
     
 }
