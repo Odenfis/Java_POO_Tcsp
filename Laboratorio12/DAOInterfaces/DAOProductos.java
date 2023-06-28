d756a62e778793759c415fcee20acd3196ecfa17
Odenfis <105258251+Odenfis@users.noreply.github.com>
2023-06-28 10:14:16 -0500

    version3

--- Laboratorio12/DAOInterfaces/DAOProductos.java
+++ Laboratorio12/DAOInterfaces/DAOProductos.java
@@ -1,14 +1,18 @@
-package DaoInterfaces;
-import DaoProyecto.Productos;
-import java.awt.event.MouseEvent;
+
+package DAO.Interfaces;
+import DAO.Proyecto.Producto;
 import javax.swing.JTable;
 
-public interface DaoProductos {
+public interface DAOProducto {
     
-    public void registrar(Productos prod) throws Exception;
-    public String mostrar() throws Exception;   
+    public void registrar(Producto p) throws Exception;
+    public void mostrar(JTable visor) throws Exception;
+    public String numcodigo() throws Exception;
+    public void actualizar(Producto p) throws Exception;    
+    
+    //
     public void eliminar() throws Exception;  
     public void buscar() throws Exception;
-    public void actualizar() throws Exception;
+    
     
 }
